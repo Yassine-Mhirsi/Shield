aec36ddacd71620ed026002a9e456cd106b33476
Yassine Mhirsi <mhirsiy@gmail.com>
2024-04-30 11:15:42 +0100

    added getallusers

--- Backend/index.ts
+++ Backend/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./src/routes/MyUserRoute";
+import myUserRoute from "./routes/MyUserRoutes";
 
 mongoose
   .connect(process.env.MONGO_URI as string)
